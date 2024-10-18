ef2ad9f150b00c464929a15916f6613d58c8d64d
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-18 16:07:48 +0530

    Added fetch & useState for  advice

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [advice, setAdvice] = useState("Please Click Button to Get Advice")
   const [count, setCount] = useState(0)
+
+   let get_advice = async () => {
+    let res = await fetch("https://api.adviceslip.com/advice",  { cache: "no-cache" });
+    let resp = await res.json();
+    setAdvice(resp.slip.advice)
+    setCount(count+1)
+   }
+
+useEffect(()=>{
+  get_advice()
+},[])
 
   return (
     <>
     <div className="container">
-      <div className="advice">Be strong . Have a nice day</div>
+      <div className="advice">{advice}</div>
       <div className="center">
-      <button>Get Advice</button>
+      <button onClick={get_advice}>Get Advice</button>
       </div>
-      <div className="n-advice">You have read pieces of advice</div>
+      <div className="n-advice">You have read {count} pieces of advice</div>
     </div>
     </>
   )
